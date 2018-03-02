5ce1aa6c15f9df14e82bbcbb11996d2fd4c45d2e
mmorozink1029 <mattmorozink@gmail.com>
2018-03-02 15:15:24 -0700

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ AdminUser.create(email: "myadmin@test.com", password: "password", password_confi
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "Holy FUCK 100 posts were just made!!"
