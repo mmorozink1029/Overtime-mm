c7003464976a8e56c5968aa42f7bf31ae60b0059
mmorozink1029 <mattmorozink@gmail.com>
2018-03-05 14:22:29 -0700

    Added phone number validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,26 @@
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "test@test.com", 
+                    password: "asdfasdf", 
+                    password_confirmation: "asdfasdf", 
+                    first_name: "Jon", 
+                    last_name: "Snow"
+                    phone: "2242191476")
 
 puts "1 user created"
 
-AdminUser.create(email: "myadmin@test.com", password: "password", password_confirmation: "password", first_name: "Matt", last_name: "Mo")
+AdminUser.create(email: "myadmin@test.com", 
+                 password: "password", 
+                 password_confirmation: "password", 
+                 first_name: "Matt", 
+                 last_name: "Mo"
+                 phone: "2242191476")
 
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, 
+               rationale: "#{post} rationale content", 
+               user_id: @user.id, 
+               overtime_request: 2.5)
 end
 
-puts "Holy FUCK 100 posts were just made!!"
+puts "100 posts were just made"
---
+++ lib/tasks/notification.rake
@@ -0,0 +1,17 @@
+namespace :notification do
+  desc "Sends SMS notification to employees asking them to log if they had overtime or not"
+  task sms: :environment do
+    # 1. Schedule to run at Sunday at 5pm
+    # 2. Iterate over all employees
+    # 3. Skip AdminUsers
+    # 4. Send a message that has instructions and a link to log time
+    # User.all.each do |user|
+    #   SmsTool.send_sms()
+    # end
+    # number: "224-219-1479"
+    # number: "2242191479"
+    # No spaces or dashes
+    # exactly 10 characters
+    # all characters have to be a number
+  end
+end
