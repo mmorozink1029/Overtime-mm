ee937ede9e29a00e2b016382999d9530d3e5f6df
mmorozink1029 <mattmorozink@gmail.com>
2018-03-06 13:57:25 -0700

    Audit Log Feature Using the Resource Generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf", 
                     first_name: "Jon", 
-                    last_name: "Snow"
-                    phone: "2242191476")
+                    last_name: "Snow",
+                    phone: "2242191479")  
 
 puts "1 user created"
 
@@ -11,8 +11,8 @@ AdminUser.create(email: "myadmin@test.com",
                  password: "password", 
                  password_confirmation: "password", 
                  first_name: "Matt", 
-                 last_name: "Mo"
-                 phone: "2242191476")
+                 last_name: "Mo",
+                  phone: "2242191479")
 
 puts "1 Admin User created"
 
