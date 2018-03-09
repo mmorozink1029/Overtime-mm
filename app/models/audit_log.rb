7460c822bcbbf11effe1dd4a1200c6abf720e150
mmorozink1029 <mattmorozink@gmail.com>
2018-03-09 13:39:39 -0700

    final changes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,5 +1,5 @@
-class AuditLog < ApplicationRecord
-  enum status: {pending: 0, confirmed: 1 }
+class AuditLog < ActiveRecord::Base
+  enum status: { pending: 0, confirmed: 1 }
 
   belongs_to :user
 
@@ -7,7 +7,13 @@ class AuditLog < ApplicationRecord
 
   after_initialize :set_defaults
 
+  before_update :set_end_date, if: :confirmed?
+
+
   private
+    def set_end_date
+      self.end_date = Date.today
+    end
 
     def set_defaults
       self.start_date ||= Date.today - 6.days
